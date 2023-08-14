1cb7069d47e8e2232cfdc8a3943233dd14b66712
yadi-90 <126985685+yadi-90@users.noreply.github.com>
2023-08-14 15:46:44 -0600

    added number to list

--- first-project/src/components/listInstructions.js
+++ first-project/src/components/listInstructions.js
@@ -4,7 +4,7 @@ function listInstructions(props) {
   return (
     <div>
     <ul>
-        <li>Decide who will be "X" and who will be "O." Player 1 typically plays as "X," and Player 2 plays as "O."</li>
+        <li>1. Decide who will be "X" and who will be "O." Player 1 typically plays as "X," and Player 2 plays as "O."</li>
     </ul>
     </div>
   )
