7673a0de52d67bfe1c32a1e6781d75ecf131f17a
yadi-90 <126985685+yadi-90@users.noreply.github.com>
2023-08-14 16:31:58 -0600

    making changes to cells to add more squares

--- first-project/src/App.js
+++ first-project/src/App.js
@@ -2,8 +2,10 @@ import React from 'react';
 import ListInstructions from './components/listInstructions';
 import './App.css';
 import Cells from './components/Cells';
+import { useState } from 'react';
 
 function App() {
+  const[cells, setCells]= useState(["","","","","","","","","",])
   return (
     <div className="App">
       <h1> Tic Tac Toe Game</h1>
@@ -13,6 +15,7 @@ function App() {
          <h2>Instructions</h2>
          <ListInstructions />
          <div className="gameBoard">
+          {Cells.map((Cells,index) => <Cells key={index} />)}
          <Cells />
          </div>
         
