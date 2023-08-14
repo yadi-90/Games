8885e0d6346bb8c6f9d1a3e693b3bfe0a67f96b4
yadi-90 <126985685+yadi-90@users.noreply.github.com>
2023-08-14 15:33:52 -0600

    making instructions

--- first-project/src/App.js
+++ first-project/src/App.js
@@ -4,7 +4,10 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1> Hellooooooooo</h1>
+      <h1> Tic Tac Toe Game</h1>
+      <p>Tic Tac Toe, also known as "Noughts and Crosses," is a two-player game where 
+        the objective is to be the first to get three of your own symbols in a row,
+         either horizontally, vertically, or diagonally, on a 3x3 grid.</p>
     </div>
   );
 }
