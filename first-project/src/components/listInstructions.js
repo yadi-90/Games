9c69d958d31b7b7c31d6a25cda4522a34b107033
yadi-90 <126985685+yadi-90@users.noreply.github.com>
2023-08-14 15:50:29 -0600

    Delete numbers I had previously place on list

--- first-project/src/components/listInstructions.js
+++ first-project/src/components/listInstructions.js
@@ -4,10 +4,10 @@ function listInstructions(props) {
   return (
     <div>
     <ol>
-        <li>1. Decide who will be "X" and who will be "O." Player 1 typically plays as "X," and Player 2 plays as "O."</li>
-        <li> 2. Players take turns making a move. Player 1 starts by placing their symbol ("X") in an empty cell on the grid.</li>
-        <li>3. Player 2 follows by placing their symbol ("O") in an empty cell.</li>
-        <li>4. </li>
+        <li>Decide who will be "X" and who will be "O." Player 1 typically plays as "X," and Player 2 plays as "O."</li>
+        <li>Players take turns making a move. Player 1 starts by placing their symbol ("X") in an empty cell on the grid.</li>
+        <li>Player 2 follows by placing their symbol ("O") in an empty cell.</li>
+        <li></li>
     </ol>
     </div>
   )
