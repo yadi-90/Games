8d0992ce9d84483d691f94fe74b9a7767280f78a
yadi-90 <126985685+yadi-90@users.noreply.github.com>
2023-08-14 15:36:57 -0600

    making instructions

--- first-project/src/App.js
+++ first-project/src/App.js
@@ -8,6 +8,8 @@ function App() {
       <p>Tic Tac Toe, also known as "Noughts and Crosses," is a two-player game where 
         the objective is to be the first to get three of your own symbols in a row,
          either horizontally, vertically, or diagonally, on a 3x3 grid.</p>
+         <h2>Instructions</h2>
+         <listInstructions />
     </div>
   );
 }
