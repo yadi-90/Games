25f1d4b715492b68bd86a2b88de05f20495dc49e
yadi-90 <126985685+yadi-90@users.noreply.github.com>
2023-08-14 15:47:31 -0600

    add step 2 in instruction list

--- first-project/src/components/listInstructions.js
+++ first-project/src/components/listInstructions.js
@@ -5,6 +5,7 @@ function listInstructions(props) {
     <div>
     <ul>
         <li>1. Decide who will be "X" and who will be "O." Player 1 typically plays as "X," and Player 2 plays as "O."</li>
+        <li> 2. Players take turns making a move. Player 1 starts by placing their symbol ("X") in an empty cell on the grid.</li>
     </ul>
     </div>
   )
