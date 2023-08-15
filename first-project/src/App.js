66f23820157073413ca7f946855cb4bf9855fb8c
yadi-90 <126985685+yadi-90@users.noreply.github.com>
2023-08-14 18:28:38 -0600

    styling page

--- first-project/src/App.js
+++ first-project/src/App.js
@@ -8,7 +8,7 @@ function App() {
   const[cells, setCells]= useState(["","","","","","","","","",])
   return (
     <div className="App">
-      <h1> Tic Tac Toe Game</h1>
+      <h1 className="Title"> Tic Tac Toe Game</h1>
       <p>Tic Tac Toe, also known as "Noughts and Crosses," is a two-player game where 
         the objective is to be the first to get three of your own symbols in a row,
          either horizontally, vertically, or diagonally, on a 3x3 grid.</p>
